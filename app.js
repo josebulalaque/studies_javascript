1e66c23094f565002ea2d8e42faa856e2757803a
Jojo <josebulalaque@yahoo.com>
2023-04-21 23:30:28 +0800

    searching DOM element by document.getElementbyID function

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // console.dir(document)
 
-document.body.children[1].children[0].href = 'https://google.com';
+//document.body.children[1].children[0].href = 'https://google.com';
+
+let anchorElement = document.getElementById('external-link');
+anchorElement.href  = 'https://google.com'
 
